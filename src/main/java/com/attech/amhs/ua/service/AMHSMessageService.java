35a359feec6cacd333c9613355fbc9a9d9a2b91d
h4rr0w1n <h4rr0w1n@gmail.com>
2026-06-23 14:14:28 +0700

    Revert "Update CTSW011.properties"
    This reverts commit 480572e9c664c1107e3b572c9eaa481a7be5064b.

--- src/main/java/com/attech/amhs/ua/service/AMHSMessageService.java
+++ src/main/java/com/attech/amhs/ua/service/AMHSMessageService.java
@@ -106,18 +106,7 @@ public class AMHSMessageService {
                 session = p7Session;
             }
 
-            // session.SetSummarizeOnBind(false);
-            // Set connection timeout before binding - use a longer timeout for initial
-            // connection
-            // try {
-            // Increase timeout to 90 seconds for more reliable connections
-            // int effectiveTimeout = Math.max(connectTimeoutSeconds, 90);
-            // session.SetTimeout(effectiveTimeout);
-            // System.out.println("DEBUG: Connection timeout set to " + effectiveTimeout + "
-            // seconds");
-            // } catch (Exception e) {
-            // System.out.println("DEBUG: Could not set timeout: " + e.getMessage());
-            // }
+            session.SetSummarizeOnBind(false);
 
             System.out.println("DEBUG: Calling bind()...");
             session.bind();
